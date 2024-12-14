b8c53ac76587c59039dab9acd8e4489c1ade55fd
Pascalis Sandi <pascalis.sandi09@gmail.com>
2024-12-14 13:11:18 +0700

    fix bottom overflow

--- attend_it/lib/pages/approving_page.dart
+++ attend_it/lib/pages/approving_page.dart
@@ -7,7 +7,9 @@ class ApprovingPage extends StatefulWidget {
   final String imagePath;
   final Schedule selectedSchedule;
 
-  const ApprovingPage({Key? key, required this.imagePath, required this.selectedSchedule}) : super(key: key);
+  const ApprovingPage(
+      {Key? key, required this.imagePath, required this.selectedSchedule})
+      : super(key: key);
 
   @override
   _ApprovingPageState createState() => _ApprovingPageState();
@@ -23,123 +25,129 @@ class _ApprovingPageState extends State<ApprovingPage> {
         title: const Text('Captured Picture'),
         backgroundColor: const Color(0xFF0047AB),
       ),
-      body: Column(
-        children: [
-          const SizedBox(height: 20), // Add spacing
-          Card(
-            margin: const EdgeInsets.symmetric(horizontal: 20),
-            elevation: 4,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(12),
-            ),
-            child: Column(
-              children: [
-                ClipRRect(
-                  borderRadius: const BorderRadius.vertical(
-                    top: Radius.circular(12),
-                  ),
-                  child: Image.file(
-                    File(widget.imagePath),
-                    height: 250,
-                    width: double.infinity,
-                    fit: BoxFit.cover,
-                  ),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              const SizedBox(height: 20), // Add spacing
+              Card(
+                margin: const EdgeInsets.symmetric(horizontal: 20),
+                elevation: 4,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12),
                 ),
-                Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Row(
+                child: Column(
+                  children: [
+                    ClipRRect(
+                      borderRadius: const BorderRadius.vertical(
+                        top: Radius.circular(12),
+                      ),
+                      child: Image.file(
+                        File(widget.imagePath),
+                        height: 250,
+                        width: double.infinity,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
-                          const Icon(Icons.access_time, size: 16),
-                          const SizedBox(width: 8),
+                          Row(
+                            children: [
+                              const Icon(Icons.access_time, size: 16),
+                              const SizedBox(width: 8),
+                              Text(
+                                // get tanggal dan waktu tepat melakukan pengambilan gambar
+                                '07:15:50 11-11-2021',
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 14,
+                                  color: Colors.grey[800],
+                                ),
+                              ),
+                            ],
+                          ),
+                          const SizedBox(height: 8),
                           Text(
-                            // get tanggal dan waktu tepat melakukan pengambilan gambar
-                            '07:15:50 11-11-2021',
+                            'Jam Kehadiran',
                             style: TextStyle(
                               fontWeight: FontWeight.bold,
-                              fontSize: 14,
+                              fontSize: 16,
                               color: Colors.grey[800],
                             ),
                           ),
+                          const SizedBox(height: 4),
+                          const Text(
+                            // get waktu melakukan pengambilan gambar
+                            '06:50:45 WIB',
+                            style: TextStyle(fontSize: 14, color: Colors.black),
+                          ),
+                          const SizedBox(height: 8),
+                          Text(
+                            'Waktu Terlambat',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 16,
+                              color: Colors.grey[800],
+                            ),
+                          ),
+                          const SizedBox(height: 4),
+                          const Text(
+                            // perhitungan set waktuMulai - waktu pengambilan gambar
+                            '00:00:00',
+                            style: TextStyle(fontSize: 14, color: Colors.black),
+                          ),
+                          Text(
+                            'Mata Kuliah',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 16,
+                              color: Colors.grey[800],
+                            ),
+                          ),
+                          const SizedBox(height: 4),
+                          Text(
+                            widget.selectedSchedule.namaMatkul,
+                            style: const TextStyle(
+                                fontSize: 14, color: Colors.black),
+                          ),
+                          Text(
+                            'Dosen',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 16,
+                              color: Colors.grey[800],
+                            ),
+                          ),
+                          const SizedBox(height: 4),
+                          Text(
+                            widget.selectedSchedule.namaDosen,
+                            style: const TextStyle(
+                                fontSize: 14, color: Colors.black),
+                          ),
                         ],
                       ),
-                      const SizedBox(height: 8),
-                      Text(
-                        'Jam Kehadiran',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 16,
-                          color: Colors.grey[800],
-                        ),
-                      ),
-                      const SizedBox(height: 4),
-                      const Text(
-                        // get waktu melakukan pengambilan gambar
-                        '06:50:45 WIB',
-                        style: TextStyle(fontSize: 14, color: Colors.black),
-                      ),
-                      const SizedBox(height: 8),
-                      Text(
-                        'Waktu Terlambat',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 16,
-                          color: Colors.grey[800],
-                        ),
-                      ),
-                      const SizedBox(height: 4),
-                      const Text(
-                        // perhitungan set waktuMulai - waktu pengambilan gambar
-                        '00:00:00',
-                        style: TextStyle(fontSize: 14, color: Colors.black),
-                      ),
-                      Text(
-                        'Mata Kuliah',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 16,
-                          color: Colors.grey[800],
-                        ),
-                      ),
-                      const SizedBox(height: 4),
-                      Text(
-                        widget.selectedSchedule.namaMatkul,
-                        style: const TextStyle(fontSize: 14, color: Colors.black),
-                      ),
-                      Text(
-                        'Dosen',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 16,
-                          color: Colors.grey[800],
-                        ),
-                      ),
-                      const SizedBox(height: 4),
-                      Text(
-                        widget.selectedSchedule.namaDosen,
-                        style: const TextStyle(fontSize: 14, color: Colors.black),
-                      ),
-                    ],
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 20), // Add spacing
+              const Center(
+                child: CircleAvatar(
+                  radius: 40,
+                  backgroundColor: Colors.green,
+                  child: Icon(
+                    Icons.check,
+                    color: Colors.white,
+                    size: 40,
                   ),
                 ),
-              ],
-            ),
-          ),
-          const SizedBox(height: 20), // Add spacing
-          const Center(
-            child: CircleAvatar(
-              radius: 40,
-              backgroundColor: Colors.green,
-              child: Icon(
-                Icons.check,
-                color: Colors.white,
-                size: 40,
               ),
-            ),
+            ],
           ),
-        ],
+        ),
       ),
       bottomNavigationBar: BottomNavBarWidget(
         currentIndex: _currentIndex,
